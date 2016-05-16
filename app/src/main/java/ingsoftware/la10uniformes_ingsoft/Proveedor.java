f4c4dce8f063f04435f8e07e9369b25afacf1d38
SZJohanR4 <johan_szr@hotmail.com>
2016-05-15 21:38:44 -0500

    codigo de johan

---
+++ app/src/main/java/ingsoftware/la10uniformes_ingsoft/Proveedor.java
@@ -0,0 +1,11 @@
+package ingsoftware.la10uniformes_ingsoft;
+
+/**
+ * Created by Desktop on 13/05/2016.
+ */
+public class Proveedor extends Usuario {
+
+    public Proveedor(String idUsuario, String nombre, String documento, String email, String contraseña, String rol) {
+        super(idUsuario, nombre, documento, email, contraseña, rol);
+    }
+}
